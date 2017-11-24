74f4e698bd043e04035c8bfcd1f7952074630ff3
Jakob Rolik <daidaloskvintus@gmail.com>
2017-11-24 18:58:30 +0100

    Update
    + login now works
    + added funtionality to register users ( only users with privilege 5 can )
    + many new templates
    + new logo
    + new favicon

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from flask import Flask, flash, redirect, render_template, request, session, url_for, jsonify, make_response, Response
+from passlib.hash import pbkdf2_sha256 as sha256
 from sqlalchemy.sql.functions import func
-from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
-from functools import wraps
-import json
-from passlib.apps import custom_app_context as pwd_context
 from assets.custom import CustomFlask 
+from flask_session import Session
+from tempfile import mkdtemp
+from functools import wraps
+import urllib.request
+import json
 
 db = SQLAlchemy()
 app = CustomFlask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///assets/users.db'
 db.init_app(app)
 
+app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.secret_key = 'super secret key'
 app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
@@ -20,50 +23,69 @@ Session(app)
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if session.get('user_id') is None:
+        if session.get('user') is None:
             return redirect(url_for('login', next=request.url))
         return f(*args, **kwargs)
     return decorated_function
 
+@app.route('/register', methods=['POST', 'GET'])
+@login_required
+def register():
+    if request.method == 'GET':
+        if session['user']['privilege'] == 5:
+            return render_template('register.html')
+        else:
+            return render_template('apology.html', message = "We are sorry but you don't have a permission to access this site.")
+    elif request.method == 'POST':
+        # TODO bude treba spravit checking
+        print(request.form.get('password'))
+        db.engine.execute('INSERT INTO users("username", "hash", "privilege") VALUES(:username, :hashh, :privilege)', {'username' : request.form.get('username'), 'hashh': sha256.hash(request.form.get('password')), 'privilege': request.form.get('privilege')})
+        return redirect( url_for('login') )
 
 @app.route('/')
 @login_required
 def index():
-    return "sup"
+    response = None
+    with urllib.request.urlopen("https://stud-db-api.herokuapp.com/api/students") as url:
+        response = json.loads(url.read().decode())
+    
+    return render_template('listStudents.html', ala=response['students'])
 
 # index 
-@app.route('/login')
+@app.route('/login', methods=["POST", "GET"])
 def login():
     """Log user in."""
-
     # forget any user_id
     session.clear()
 
     # if user reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-
         # ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username")
+            return redirect(render_template('login.html', message='No usename provided!'))
 
         # ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password")
+            return render_template('login.html', message='No password provided!')
 
         # query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username"))
+        rows = db.engine.execute("SELECT * FROM users WHERE username = :username", {'username': request.form.get('username')})
+
+        ourBoy = rows.first()
 
         # ensure username exists and password is correct
-        if len(rows) != 1 or not pwd_context.verify(request.form.get("password"), rows[0]["hash"]):
-            return apology("invalid username and/or password")
+        if not rows or not sha256.verify(request.form.get("password"), ourBoy[2]):
+            return render_template('login.html', message='Invalid username of password')
 
         # remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        session['user'] = {
+            'user_id': ourBoy[0],
+            'username': ourBoy[1],
+            'privilege': ourBoy[3] 
+        }
 
         # redirect user to home page
         return redirect(url_for("index"))
-
-    # else if user reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
 
